716f88ecfbf6423e8448cf4a5e1c4e50f3311f1f
JohnLau55 <1277562191@qq.com>
2021-05-15 11:47:35 +0800

    add three sout again

--- WeChat Phone Book/src/main/java/com/mingrisoft/MainActivity.java
+++ WeChat Phone Book/src/main/java/com/mingrisoft/MainActivity.java
@@ -46,5 +46,8 @@ public class MainActivity extends Activity {
         System.out.println("test3");
         System.out.println("test4");
         System.out.println("test5");
+        System.out.println("test6");
+        System.out.println("test7");
+        System.out.println("test8");
     }
 }
